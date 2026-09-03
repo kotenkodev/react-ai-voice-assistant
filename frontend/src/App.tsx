0f9e6f02645d0fb8937bb48926376bf0f01e5fab
Vadym <vadym.kotenko.dev@gmail.com>
2026-09-03 14:14:19 +0300

    feat: render Controller component in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
+import Controller from "./components/Controller";
+
 function App() {
   return (
     <main>
-      <h1 className="text-4xl text-blue-600 font-bold text-center">
-        Virtual Assistant
-      </h1>
-      <div className="flex flex-col items-center gap-4">
-        <div className="flex flex-col items-center gap-2">
-          <p>Listening</p>
-          <button>Record</button>
-        </div>
-        <div>
-          <p>Not Listening</p>
-          <button>Stop</button>
-        </div>
-      </div>
+      <Controller />
     </main>
   );
 }
